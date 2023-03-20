3ecca52598088654a4649f983b75c274e0263f61
Ayman-Kaddar <71832197+Ayman-Kaddar@users.noreply.github.com>
2023-03-21 00:22:39 +0100

    Dev2

---
+++ Login/resources/views/template/template.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login & Register</title>
+        @stack('styles')
+    </head>
+    <body>
+        @yield('content')
+
+        <script src="https://code.jquery.com/jquery-3.6.1.slim.js" integrity="sha256-tXm+sa1uzsbFnbXt8GJqsgi2Tw+m4BLGDof6eUPjbtk=" crossorigin="anonymous"></script>
+        @stack('scripts')
+    </body>
+</html>
